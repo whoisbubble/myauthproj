52da651677fa7f5f38710147f78d5ae41295124d
Лягушка Чигирёв <bananov.timur@yandex.ru>
2026-02-03 18:37:55 +1000

    feat(web): init next.js app

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">My Page</h1>
+      <p className="mt-2 text-white-600">Lesson 1</p>
+    </main>
+  )
+}
